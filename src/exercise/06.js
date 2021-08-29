5080856e7a6feb0abf6af984eca9cb8fb1a39fb3
Diego Delgadillo Alvarez <dgo2389@hotmail.com>
2021-08-29 15:07:41 -0500

    06-check

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -8,7 +8,7 @@ function UsernameForm({onSubmitUsername}) {
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+  // 📜 ht<tps://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
   //
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
@@ -19,13 +19,55 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const [hasError, setHasError] = React.useState(false);
+  const [userName, setUserName] = React.useState('');
+  const inputRef = React.useRef(null);
+
+  function handleChange(e){
+    e.preventDefault();
+    const value = inputRef.current.value;
+    const isValid = value === value.toLowerCase();
+    setHasError(!isValid);
+    if (isValid) {
+      setUserName(value);
+    }
+    console.log(value, isValid);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    // console.log(e.target);
+    console.log(e.target.elements);
+    console.log(e.target.elements.inputUserName);
+    console.log(e.target.elements.inputUserName.value);
+    // console.log(e.target.elements[0]);
+    // console.log(e.target.elements[0].value);
+    console.log(inputRef.current.value);
+    onSubmitUsername(inputRef.current.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="inputUserName">Username:</label>
+        <input id="inputUserName" type="text" ref={inputRef} onChange={handleChange} value={userName} />
       </div>
-      <button type="submit">Submit</button>
+      {hasError && (
+        <span role="alert" style={{color: 'red', display: 'block'}}>
+          Username must be lower case!
+        </span>
+      )}
+      <button disabled={Boolean(hasError)} type="submit">
+        Submit
+      </button>
+      {/* {hasError ? (
+        <>
+          <button type="submit" disabled>Submit</button>
+          <span>Username must be lower case!</span>
+        </>
+      ) : (
+        <button type="submit">Submit</button>
+      )} */}
     </form>
   )
 }
